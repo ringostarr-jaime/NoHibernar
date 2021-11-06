0ae334e301663eb25e98ed900c936d39e56f6c63
ringostarr-jaime <70113275+ringostarr-jaime@users.noreply.github.com>
2021-11-05 23:18:43 -0600

    primer commit

---
+++ MovimientoMouse/src/main/java/com/jaime/movimientomouse/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jaime.movimientomouse;
+
+/**
+ *
+ * @author Jymmy
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ventana v = new ventana();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+       
+    }
+
+}
